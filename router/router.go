cd65e2ea7230d2b1a8a6dea6b34d7ef2c04d0f23
Erik Campobadal <soc@erik.cat>
2019-03-30 16:08:01 +0100

    Added all HTTP methods

--- router/router.go
+++ router/router.go
@@ -45,8 +45,8 @@ func CreateWithOptions(options *Options) *Router {
 	return r
 }
 
-// Get creates a GET route.
-func (r *Router) Get(uri string, handler func(req *request.HTTP) response.HTTP) {
+// Adds the route to the given router.
+func addRoute(r *Router, uri string, handler Handler, method request.Type) {
 	h := handler
 	if r.options.Middleware != nil {
 		h = r.options.Middleware(handler)
@@ -54,8 +54,38 @@ func (r *Router) Get(uri string, handler func(req *request.HTTP) response.HTTP) 
 	// Append the route to the list.
 	r.Routes = append(
 		r.Routes,
-		Route{URI: r.options.Prefix + uri, Handler: h},
+		Route{URI: r.options.Prefix + uri, Method: method, Handler: h},
 	)
+}
+
+// Get creates a GET route.
+func (r *Router) Get(uri string, handler Handler) {
+	addRoute(r, uri, handler, request.GetRequest)
+}
+
+// Head creates a HEAD route.
+func (r *Router) Head(uri string, handler Handler) {
+	addRoute(r, uri, handler, request.HeadRequest)
+}
+
+// Post creates a POST route.
+func (r *Router) Post(uri string, handler Handler) {
+	addRoute(r, uri, handler, request.PostRequest)
+}
+
+// Put creates a PUT route.
+func (r *Router) Put(uri string, handler Handler) {
+	addRoute(r, uri, handler, request.PutRequest)
+}
+
+// Patch creates a PATCH route.
+func (r *Router) Patch(uri string, handler Handler) {
+	addRoute(r, uri, handler, request.PatchRequest)
+}
+
+// Delete creates a DELETE route.
+func (r *Router) Delete(uri string, handler Handler) {
+	addRoute(r, uri, handler, request.DeleteRequest)
 }
 
 // Group certain routes uner certain options.
